307999c86a2c859a598a6a0f68b4087227a48a5e
square_heron942 <82814680+Squareheron942@users.noreply.github.com>
2022-11-07 14:48:47 -0500

    Initial commit

--- Unit 5/01/Main.java
+++ Unit 5/01/Main.java
@@ -12,6 +12,27 @@ public class Main {
             sample[i] = Integer.parseInt(s[i]);
         }
 
+        int a = sample[0];
+        int b = sample[sample.length-1];
+
+        sample[0] = b;
+        sample[sample.length-1] = a;
+
+        for (int i = 0; i < sample.length; i++) {
+            sample[i] = Math.abs(sample[i]);
+        }
+
+        int sampleSum = 0;
+
+        for (int i = 0; i < sample.length; i++) {
+            sampleSum += sample[i];
+        }
+
+        System.out.println(sampleSum);
+
+        for (int i = 0; i < sample.length; i+=2) {
+            System.out.print(sample[i] + " ");
+        }
 
     }
 }
