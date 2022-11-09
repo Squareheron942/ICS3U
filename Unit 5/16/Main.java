c94967b75c49168d4e3887e6cb68fb1cc160a237
square_heron942 <82814680+Squareheron942@users.noreply.github.com>
2022-11-09 18:38:11 -0500

    Additional questions

--- Unit 5/16/Main.java
+++ Unit 5/16/Main.java
@@ -21,21 +21,22 @@ public class Main {
         for (int i = 0; i < n; i++) {
             p.put(l1[i], l2[i]);
         }
+        
+        if (!l1[0].equals("Ada") && !l1[0].equals("Rich")) {
+            // System.out.println(Arrays.toString(l1) + "\n" + Arrays.toString(l2));
+        }
 
         // System.out.println(Arrays.toString(l1) + "\n" + Arrays.toString(l2) + "\n" + p);
 
         int checker = 0;
         for (int i = 0; i < n; i++) {
-            if (p.get(l1[i]) == l1[i]) {checker++;}
+            if (p.get(l1[i]).equals(l1[i])) {checker++;}
             if (p.get(l1[i]).equals(getKeyByValue(p, l1[i]))) {
             } else {
                 checker++;
             }
         }
-
         System.out.println(checker == 0 ? "good" : "bad");
-
-
     }
 
     public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
