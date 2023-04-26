a7a7c840f4dd3fc714544a266b3bfcd260af0830
square_heron942 <82814680+Squareheron942@users.noreply.github.com>
2023-04-26 09:53:58 -0400

    Test a cube

--- Final Project (GUI)/Main.java
+++ Final Project (GUI)/Main.java
@@ -3,40 +3,137 @@ public class Main {
 
         Scene scene = new Scene();       
 
+        // scene.add(
+        //     new GameObject(
+        //         new Mesh(
+        //             new Triangle[] {
+        //                 new Triangle(
+        //                     new Vertex[] {
+        //                         new Vertex(new Vector3(100, 100, 100), new Color(1, 0, 0)),
+        //                         new Vertex(new Vector3(-100, -100, 100), new Color(0, 0, 1)),
+        //                         new Vertex(new Vector3(-100, 100, -100), new Color(1, 1, 1))
+        //                     }
+        //                 ),
+        //                 new Triangle(
+        //                     new Vertex[] {
+        //                         new Vertex(new Vector3(100, 100, 100), new Color(1, 0, 0)),
+        //                         new Vertex(new Vector3(-100, -100, 100), new Color(0, 0, 1)),
+        //                         new Vertex(new Vector3(100, -100, -100), new Color(0, 0, 1))
+        //                     }
+        //                 ),
+        //                 new Triangle(
+        //                     new Vertex[] {
+        //                         new Vertex(new Vector3(-100, 100, -100), new Color(1, 1, 1)),
+        //                         new Vertex(new Vector3(100, -100, -100), new Color(0, 1, 0)),
+        //                         new Vertex(new Vector3(100, 100, 100), new Color(1, 0, 0))
+        //                     }
+        //                 ),
+        //                 new Triangle(
+        //                     new Vertex[] {
+        //                         new Vertex(new Vector3(-100, 100, -100), new Color(1, 1, 1)),
+        //                         new Vertex(new Vector3(100, -100, -100), new Color(0, 1, 0)),
+        //                         new Vertex(new Vector3(-100, -100, 100), new Color(0, 0, 1))
+        //                     }
+        //                 )
+        //             }), 
+        //             new Material(new Color())));
+
+
         scene.add(
             new GameObject(
                 new Mesh(
                     new Triangle[] {
                         new Triangle(
-                            new Vertex[] {
-                                new Vertex(new Vector3(100, 100, 100), new Color(1, 0, 0)),
-                                new Vertex(new Vector3(-100, -100, 100), new Color(0, 0, 1)),
-                                new Vertex(new Vector3(-100, 100, -100), new Color(1, 1, 1))
+                            new Vertex[] { // back face
+                                new Vertex(new Vector3(50, -50, 50)),
+                                new Vertex(new Vector3(-50, 50, 50)),
+                                new Vertex(new Vector3(50, 50, 50))
                             }
                         ),
                         new Triangle(
                             new Vertex[] {
-                                new Vertex(new Vector3(100, 100, 100), new Color(1, 0, 0)),
-                                new Vertex(new Vector3(-100, -100, 100), new Color(0, 0, 1)),
-                                new Vertex(new Vector3(100, -100, -100), new Color(0, 0, 1))
+                                new Vertex(new Vector3(50, -50, 50)),
+                                new Vertex(new Vector3(-50, -50, 50)),
+                                new Vertex(new Vector3(-50, 50, 50))
+                            }
+                        ),
+                        new Triangle( // right face
+                            new Vertex[] {
+                                new Vertex(new Vector3(50, -50, 50)),
+                                new Vertex(new Vector3(50, -50, -50)),
+                                new Vertex(new Vector3(50, 50, -50))
                             }
                         ),
                         new Triangle(
                             new Vertex[] {
-                                new Vertex(new Vector3(-100, 100, -100), new Color(1, 1, 1)),
-                                new Vertex(new Vector3(100, -100, -100), new Color(0, 1, 0)),
-                                new Vertex(new Vector3(100, 100, 100), new Color(1, 0, 0))
+                                new Vertex(new Vector3(50, -50, 50)),
+                                new Vertex(new Vector3(50, 50, 50)),
+                                new Vertex(new Vector3(50, 50, -50))
+                            }
+                        ),
+                        new Triangle( // bottom face
+                            new Vertex[] {
+                                new Vertex(new Vector3(50, 50, 50)),
+                                new Vertex(new Vector3(-50, 50, -50)),
+                                new Vertex(new Vector3(-50, 50, 50))
                             }
                         ),
                         new Triangle(
                             new Vertex[] {
-                                new Vertex(new Vector3(-100, 100, -100), new Color(1, 1, 1)),
-                                new Vertex(new Vector3(100, -100, -100), new Color(0, 1, 0)),
-                                new Vertex(new Vector3(-100, -100, 100), new Color(0, 0, 1))
+                                new Vertex(new Vector3(50, 50, -50)),
+                                new Vertex(new Vector3(-50, 50, -50)),
+                                new Vertex(new Vector3(50, 50, 50))
                             }
-                        )
-                    }), 
-                    new Material(new Color())));
+                        ),
+                        new Triangle( // back face
+                            new Vertex[] {
+                                new Vertex(new Vector3(50, -50, -50)),
+                                new Vertex(new Vector3(-50, -50, -50)),
+                                new Vertex(new Vector3(-50, 50, -50))
+                            }
+                        ),
+                        new Triangle(
+                            new Vertex[] {
+                                new Vertex(new Vector3(-50, -50, -50)),
+                                new Vertex(new Vector3(-50, 50, -50)),
+                                new Vertex(new Vector3(50, 50, -50))
+                            }
+                        ),
+                        new Triangle( // top face
+                            new Vertex[] {
+                                new Vertex(new Vector3(-50, -50, 50)),
+                                new Vertex(new Vector3(50, -50, 50)),
+                                new Vertex(new Vector3(50, -50, -50))
+                            }
+                        ),
+                        new Triangle(
+                            new Vertex[] {
+                                new Vertex(new Vector3(-50, -50, 50)),
+                                new Vertex(new Vector3(-50, -50, -50)),
+                                new Vertex(new Vector3(50, -50, -50))
+                            }
+                        ),
+                        new Triangle( // left face
+                            new Vertex[] {
+                                new Vertex(new Vector3(-50, -50, 50)),
+                                new Vertex(new Vector3(-50, 50, 50)),
+                                new Vertex(new Vector3(-50, -50, -50))
+                            }
+                        ),
+                        new Triangle(
+                            new Vertex[] {
+                                new Vertex(new Vector3(-50, -50, -50)),
+                                new Vertex(new Vector3(-50, 50, 50)),
+                                new Vertex(new Vector3(-50, 50, -50))
+                            }
+                        ),
+                    }
+                ),
+                new Material(
+                    new Color()
+                )
+            )
+        );
 
         PerspectiveCamera cam = new PerspectiveCamera(
             new Vector3(0, 0, 0), // position
